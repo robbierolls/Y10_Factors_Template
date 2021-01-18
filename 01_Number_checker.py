996d77327e5f869f44c9fbdbc4bf9c090bb7f8ae
Jennifer Gottschalk <jgottschalk@masseyhigh.school.nz>
2021-01-18 14:49:18 +1300

    Create 01_Number_checker.py

---
+++ 01_Number_checker.py
@@ -0,0 +1,44 @@
+# Integer checking function
+
+# Number checking function goes here
+def intcheck(question, low=None, high=None):
+
+    while True:
+
+        # sets up error messages
+        if low is not None and high is not None:
+            error = "Please enter an integer between {} and {} (inclusive)".format(low, high)
+        elif low is not None and high is None:
+            error = "Please enter an integer that is more than or equal to {}".format(low)
+        elif low is None and high is not None:
+            error = "Please enter an integer that is less than or equal to {}".format(high)
+        else:
+            error = "Please enter an integer"
+
+        try:
+            response = int(input(question))
+
+            # Checks response is not too low
+            if low is not None and response < low:
+                print(error)
+                continue
+
+            # Checks response is not too high
+            if high is not None and response > high:
+                print(error)
+                continue
+
+            return response
+
+        # checks input is a integer
+        except ValueError:
+            print(error)
+            continue
+
+
+# Main routine
+
+lowest = intcheck("Low Number: ")
+highest = intcheck("High Number: ", lowest + 1)
+rounds = intcheck("Rounds: ", 1)
+guess = intcheck("Guess: ", lowest, highest)
